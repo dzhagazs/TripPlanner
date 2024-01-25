cc782fd175abe77bdfb142df3b3f9b253d3f846d
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-25 17:21:44 +0200

    Extract validation

--- TripPlannerCore/TripPlannerCoreTests/RouteBuilderTests.swift
+++ TripPlannerCore/TripPlannerCoreTests/RouteBuilderTests.swift
@@ -124,11 +124,24 @@ final class DijkstrasRouteBuilder<W: Number, E: Hashable>: RouteBuilder {
     ) async throws -> [Route<E, W>] {
 
         guard connections.isEmpty == false else { return [] }
-        guard connections.first(where: { $0.1 == to }) != nil else { throw Error.toNotFound }
-        guard connections.first(where: { $0.0 == from }) != nil else { throw Error.fromNotFound }
+        try validate(from: from, to: to, connections: connections)
 
         let weight = try await weightCalculator(connections.first!)
 
         return [.init(path: [connections.first!.0, connections.first!.1], weight: weight)]
+    }
+
+    // MARK: Private
+
+    private func validate(
+
+        from: E,
+        to: E,
+        connections: [(E, E)]
+
+    ) throws {
+
+        guard connections.first(where: { $0.1 == to }) != nil else { throw Error.toNotFound }
+        guard connections.first(where: { $0.0 == from }) != nil else { throw Error.fromNotFound }
     }
 }
