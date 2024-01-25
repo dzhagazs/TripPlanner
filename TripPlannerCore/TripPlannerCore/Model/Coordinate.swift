e4955811cf321c3e4da90728744684c308bb5750
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-25 13:51:30 +0200

    Add Coordinate

---
+++ TripPlannerCore/TripPlannerCore/Model/Coordinate.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinate.swift
+//  TripPlannerCore
+//
+//  Created by Oleksandr Vasildzhagaz on 25.01.2024.
+//
+
+struct Coordinate {
+
+    let latitude: Float
+    let longitude: Float
+}
