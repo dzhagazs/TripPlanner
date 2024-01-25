cb993a57a76940876b50bc2c1744a86401686e04
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-25 16:47:38 +0200

    Add first test

--- TripPlannerCore/TripPlannerCoreTests/RouteBuilderTests.swift
+++ TripPlannerCore/TripPlannerCoreTests/RouteBuilderTests.swift
@@ -11,22 +11,59 @@ import XCTest
 
 final class RouteBuilderTests: XCTestCase {
 
-    typealias SUT = RouteBuilder
-    typealias WeightCalculator = (Connection) async throws -> UInt
+    typealias SUT = DijkstrasRouteBuilder<Int, String>
+
+    func test_build_returnsOriginalConnectionIfPointsAreConnected() {
+
+        execute {
+
+            let sut = Self.makeSUT()
+
+            let route = try await sut.build(
+
+                from: "a",
+                to: "b",
+                connections: [("a", "b")],
+                weightCalculator: { _ in 1 }
+            )
+
+            XCTAssertEqual(route, .init(path: ["a", "b"], weight: 1))
+        }
+    }
 
     // MARK: Private
 
-    private static func makeSUT() -> some RouteBuilder {
+    private static func makeSUT() -> SUT { .init() }
+}
 
-        DijkstrasRouteBuilder<Int>()
+extension Int: Number {
+
+    public static var upperBound: Self { .max }
+    public static var zero: Self { 0 }
+}
+
+extension Route<String, Int>: Equatable {
+
+    public static func == (lhs: Route, rhs: Route) -> Bool {
+
+        lhs.path == rhs.path && lhs.weight == rhs.weight
     }
 }
 
-final class DijkstrasRouteBuilder<W: Number>: RouteBuilder {
+final class DijkstrasRouteBuilder<W: Number, E: Hashable>: RouteBuilder {
 
     typealias Weight = W
+    typealias RouteElement = E
 
-    func build(_ connections: [Connection], weightCalculator: (Connection) async throws -> Weight) async throws -> Route {
-        ([], Weight.zero)
+    func build(
+
+        from: E,
+        to: E,
+        connections: [(E, E)],
+        weightCalculator: ((E, E)) async throws -> W
+
+    ) async throws -> Route<E, W> {
+
+        .init(path: [], weight: W.zero)
     }
 }
