6b71dd2722f08b8d27d6dc30139759e4d5479778
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-25 15:13:31 +0200

    Add number constraints

--- TripPlannerCore/TripPlannerCore/RouteBuilder.swift
+++ TripPlannerCore/TripPlannerCore/RouteBuilder.swift
@@ -7,10 +7,28 @@
 
 protocol RouteBuilder {
 
-    associatedtype Weight: Equatable, Comparable
+    associatedtype Weight: Number
 
     typealias Route = ([Place], Weight)
     typealias WeightCalculator = (Connection) async throws -> Weight
 
     func build(_ connections: [Connection], weightCalculator: WeightCalculator) async throws -> Route
+}
+
+protocol Number: Comparable {
+
+    static var upperBound: Self { get }
+    static var zero: Self { get }
+}
+
+extension Int: Number {
+
+    static var upperBound: Int { .max }
+    static var zero: Int { 0 }
+}
+
+extension Float: Number {
+
+    static var upperBound: Float { .infinity }
+    public static var zero: Float { 0 }
 }
