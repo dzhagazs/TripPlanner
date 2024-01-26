44014110822daf8a74fcb1291c7640cebdfc6c89
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-26 11:35:35 +0200

    Rearrange functions

--- TripPlannerCore/TripPlannerCore/RouteBuilder/Impl/DijkstrasRouteBuilder.swift
+++ TripPlannerCore/TripPlannerCore/RouteBuilder/Impl/DijkstrasRouteBuilder.swift
@@ -25,15 +25,13 @@ internal final class DijkstrasRouteBuilder<W: Number, E: Hashable>: RouteBuilder
         try validate(from: from, to: to, connections: connections)
 
         let filteredNodes = getAllNodes(from: connections, ignored: [from])
-        let allNodes = [from] + filteredNodes
 
         let fromNeighbors = directNeighbors(for: from, with: connections)
         var weights = initialWeights(for: from, fromNeighbors: fromNeighbors, filteredNodes: filteredNodes)
         var parents = initialParents(for: from, fromNeighbors: fromNeighbors)
-
         var routes = initialRoutes(from: from, to: to, connections: connections)
 
-        let graph = try initialGraph(for: allNodes, connections: connections)
+        let graph = try initialGraph(for: [from] + filteredNodes, connections: connections)
 
         fill(
 
@@ -52,100 +50,47 @@ internal final class DijkstrasRouteBuilder<W: Number, E: Hashable>: RouteBuilder
 
     // MARK: Private
 
-    private static func fill(
-
-        with graph: [E: [E: W]],
-        from: E,
-        to: E,
-        routes: inout [Route<E, W>],
-        weights: inout [E: W],
-        parents: inout [E: E]
-    ) {
-
-        var processed: [E] = []
-        var processedElement = Self.lowestWeightElement(weights, processed: processed)
-
-        repeat {
-
-            guard let element = processedElement else { break }
-
-            let weight = weights[element]
-            let neighbors = graph[element]
-            neighbors?.keys.forEach { neighbor in
-
-                let newWeight = weight! + neighbors![neighbor]!
-                if weights[neighbor]! > newWeight {
-
-                    weights[neighbor] = newWeight
-                    parents[neighbor] = element
-                    if weights[to]! < .upperBound {
-
-                        routes.append(.init(
-
-                            path: [from] + Self.path(from: from, to: to, parents: parents),
-                            weight: weights[to]!
-                        ))
-                    }
-                }
-            }
-            processed.append(element)
-            processedElement = Self.lowestWeightElement(weights, processed: processed)
-
-        } while processedElement != nil
-    }
-
-    private static func initialGraph(for nodes: [E], connections: [Connection]) throws -> [E: [E: W]] {
-
-        var graph: [E: [E: W]] = [:]
-
-        // Fill initial graph
-        try nodes.forEach { node in
-
-            let neighbors = Self.directNeighbors(for: node, with: connections)
-            var neighborsWeights: [E: W] = [:]
-            try neighbors.forEach { neighbor in
-
-                let weight = neighbor.2
-
-                guard weight >= .zero else { throw Error.invalidWeight }
-
-                neighborsWeights[neighbor.1] = weight
-            }
-
-            graph[node] = neighborsWeights
-        }
-
-        return graph
-    }
-
-    private static func initialRoutes(from: E, to: E, connections: [Connection]) -> [Route<E, W>] {
-
-        var routes: [Route<E, W>] = []
-
-        if let direct = directConnectionRouteIfExists(from: from, to: to, connections: connections) {
-
-            routes.append(direct)
-        }
-
-        return routes
-    }
-
-    private static func directConnectionRouteIfExists(
+    private static func validate(
 
         from: E,
         to: E,
-        connections: [Connection]
+        connections: [(E, E, W)]
 
-    ) -> Route<E, W>? {
+    ) throws {
 
-        guard let directConnection = connections.first(where: { $0.0 == from && $0.1 == to }) else { return nil }
+        guard connections.count > 0 else { throw Error.notFound }
+        guard connections.first(where: { $0.1 == to }) != nil else { throw Error.toNotFound }
+        guard connections.first(where: { $0.0 == from }) != nil else { throw Error.fromNotFound }
+    }
 
-        return .init(path: [from, to], weight: directConnection.2)
+    private static func getAllNodes(from connections: [Connection], ignored: [E]) -> [E] {
+
+        let nodes = connections.reduce(NSMutableOrderedSet()) {
+
+            $0.add($1.0)
+            $0.add($1.1)
+
+            return $0
+        }
+
+        nodes.minus(NSOrderedSet(array: ignored))
+
+        return nodes.array as! [E]
+    }
+
+    private static func directNeighbors(
+
+        for element: E,
+        with connections: [Connection]
+
+    ) -> [Connection] {
+
+        connections.filter { $0.0 == element }
     }
 
     private static func initialWeights(
 
-        for from: E, 
+        for from: E,
         fromNeighbors: [Connection],
         filteredNodes: [E]
 
@@ -184,6 +129,97 @@ internal final class DijkstrasRouteBuilder<W: Number, E: Hashable>: RouteBuilder
         return parents
     }
 
+    private static func initialRoutes(from: E, to: E, connections: [Connection]) -> [Route<E, W>] {
+
+        var routes: [Route<E, W>] = []
+
+        if let direct = directConnectionRouteIfExists(from: from, to: to, connections: connections) {
+
+            routes.append(direct)
+        }
+
+        return routes
+    }
+
+    private static func initialGraph(for nodes: [E], connections: [Connection]) throws -> [E: [E: W]] {
+
+        var graph: [E: [E: W]] = [:]
+
+        // Fill initial graph
+        try nodes.forEach { node in
+
+            let neighbors = directNeighbors(for: node, with: connections)
+            var neighborsWeights: [E: W] = [:]
+            try neighbors.forEach { neighbor in
+
+                let weight = neighbor.2
+
+                guard weight >= .zero else { throw Error.invalidWeight }
+
+                neighborsWeights[neighbor.1] = weight
+            }
+
+            graph[node] = neighborsWeights
+        }
+
+        return graph
+    }
+
+    private static func fill(
+
+        with graph: [E: [E: W]],
+        from: E,
+        to: E,
+        routes: inout [Route<E, W>],
+        weights: inout [E: W],
+        parents: inout [E: E]
+    ) {
+
+        var processed: [E] = []
+        var processedElement = lowestWeightElement(weights, processed: processed)
+
+        repeat {
+
+            guard let element = processedElement else { break }
+
+            let weight = weights[element]
+            let neighbors = graph[element]
+            neighbors?.keys.forEach { neighbor in
+
+                let newWeight = weight! + neighbors![neighbor]!
+                if weights[neighbor]! > newWeight {
+
+                    weights[neighbor] = newWeight
+                    parents[neighbor] = element
+                    if weights[to]! < .upperBound {
+
+                        routes.append(.init(
+
+                            path: [from] + path(from: from, to: to, parents: parents),
+                            weight: weights[to]!
+                        ))
+                    }
+                }
+            }
+            processed.append(element)
+            processedElement = lowestWeightElement(weights, processed: processed)
+
+        } while processedElement != nil
+    }
+
+    private static func directConnectionRouteIfExists(
+
+        from: E,
+        to: E,
+        connections: [Connection]
+
+    ) -> Route<E, W>? {
+
+        guard let directConnection = connections.first(where: { $0.0 == from && $0.1 == to }) else { return nil }
+
+        return .init(path: [from, to], weight: directConnection.2)
+    }
+
     private static func lowestWeightElement(_ weights: [E: W], processed: [E]) -> E? {
 
         var lowestWeight: W = .upperBound
@@ -201,16 +237,6 @@ internal final class DijkstrasRouteBuilder<W: Number, E: Hashable>: RouteBuilder
         return result
     }
 
-    private static func directNeighbors(
-
-        for element: E,
-        with connections: [Connection]
-
-    ) -> [Connection] {
-
-        connections.filter { $0.0 == element }
-    }
-
     private static func path(from: E, to: E, parents: [E: E]) -> [E] {
 
         var path: [E] = []
@@ -223,33 +249,5 @@ internal final class DijkstrasRouteBuilder<W: Number, E: Hashable>: RouteBuilder
         } while element != from
 
         return path.reversed()
-    }
-
-    private static func validate(
-
-        from: E,
-        to: E,
-        connections: [(E, E, W)]
-
-    ) throws {
-
-        guard connections.count > 0 else { throw Error.notFound }
-        guard connections.first(where: { $0.1 == to }) != nil else { throw Error.toNotFound }
-        guard connections.first(where: { $0.0 == from }) != nil else { throw Error.fromNotFound }
-    }
-
-    private static func getAllNodes(from connections: [Connection], ignored: [E]) -> [E] {
-
-        let nodes = connections.reduce(NSMutableOrderedSet()) {
-
-            $0.add($1.0)
-            $0.add($1.1)
-
-            return $0
-        }
-
-        nodes.minus(NSOrderedSet(array: ignored))
-
-        return nodes.array as! [E]
     }
 }
