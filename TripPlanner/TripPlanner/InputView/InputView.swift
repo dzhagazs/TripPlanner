7c913a730b7954e1e1f3590e0068bec4c4a27d0c
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-26 20:25:46 +0200

    Add InputView

---
+++ TripPlanner/TripPlanner/InputView/InputView.swift
@@ -0,0 +1,62 @@
+//
+//  InputView.swift
+//  TripPlanner
+//
+//  Created by Oleksandr Vasildzhahaz on 26.01.2024.
+//
+
+import SwiftUI
+
+struct InputView: View {
+
+    @Bindable var vm: InputViewModel
+    let onEdit: (String) -> Void
+
+    var body: some View {
+
+        VStack {
+
+            TextField(vm.key, text: $vm.value)
+
+                .padding()
+
+            ZStack {
+
+                List(vm.suggestions, id: \.self) { suggestion in
+
+                    Text(suggestion)
+                }
+
+                .listStyle(.plain)
+                .opacity(vm.loading ? 0 : 1)
+
+                VStack {
+
+                    ProgressView()
+
+                        .padding()
+
+                    Spacer()
+                }
+
+                .opacity(vm.loading ? 1 : 0)
+
+            }
+        }
+
+        .onChange(of: vm.value) { withAnimation { onEdit(vm.value) } }
+    }
+}
+
+#Preview {
+
+    let vm = InputViewModel(
+
+        key: "From",
+        value: "",
+        suggestions: ["London", "Tokyo"],
+        loading: false
+    )
+
+    return InputView(vm: vm) { _ in }
+}
