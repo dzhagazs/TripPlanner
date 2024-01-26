318303518ab2ac53be8e37dbe6cff8bcf75f0227
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-26 17:12:29 +0200

    Add app and workspace

---
+++ TripPlanner/TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Alexandr Vasildzhagaz on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
