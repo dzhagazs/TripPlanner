444b595162e7cce6138874a9ae3dc935cbf25c18
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-26 22:12:09 +0200

    Add helper functions

--- TripPlannerCore/TripPlannerCoreTests/ConnectionLoaderTests.swift
+++ TripPlannerCore/TripPlannerCoreTests/ConnectionLoaderTests.swift
@@ -15,6 +15,38 @@ final class ConnectionLoaderTests: XCTestCase {
 
     // MARK: Private
 
+    private func expectToLoad(
+
+        _ connections: [Connection],
+        on sut: SUT,
+        file: StaticString = #file,
+        line: UInt = #line
+
+    ) {
+
+        execute(file: file, line: line) {
+
+            let result = try await sut.load()
+
+            XCTAssertEqual(connections, result, file: file, line: line)
+        }
+    }
+
+    private func expectToFail<E: Error & Equatable>(
+
+        _ error: E,
+        on sut: SUT,
+        file: StaticString = #file,
+        line: UInt = #line
+
+    ) {
+
+        expectToThrow(error, file: file, line: line) {
+
+            _ = try await sut.load()
+        }
+    }
+
     private func makeSUT(
 
         sourceResult: DataSourceStub.Value = .success(Data()),
