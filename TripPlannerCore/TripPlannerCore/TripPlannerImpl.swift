bf8b9ab97726c3772b41f4e60f6545ff701d7121
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-26 14:21:39 +0200

    Add empty TripPlannerImpl

---
+++ TripPlannerCore/TripPlannerCore/TripPlannerImpl.swift
@@ -0,0 +1,46 @@
+//
+//  TripPlannerImpl.swift
+//  TripPlannerCore
+//
+//  Created by Alexandr Vasildzhagaz on 26.01.2024.
+//
+
+final class TripPlannerImpl: TripPlanner {
+
+    // MARK: TripPlanner
+
+    func loadPlaces() async throws {
+
+        
+    }
+
+    func fromSuggestions(filter: String) -> [Place] {
+
+        []
+    }
+
+    func toSuggestions(filter: String) -> [Place] {
+
+        []
+    }
+
+    func select(from: Place) throws {
+
+        
+    }
+
+    func select(to: Place) throws {
+
+        
+    }
+
+    func clearSelection() {
+
+        
+    }
+
+    func build() async throws -> [PresentableRoute] {
+
+        []
+    }
+}
