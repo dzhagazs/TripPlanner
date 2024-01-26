0be95780874048b48d663ef14737e9fc39474e34
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-26 14:47:17 +0200

    Inject loader

--- TripPlannerCore/TripPlannerCore/TripPlannerImpl.swift
+++ TripPlannerCore/TripPlannerCore/TripPlannerImpl.swift
@@ -7,11 +7,18 @@
 
 final class TripPlannerImpl: TripPlanner {
 
+    typealias Loader = () async throws -> [Place]
+    typealias Error = TripPlannerError
+
     // MARK: TripPlanner
 
-    func loadPlaces() async throws {
+    func loadPlaces() async throws -> [Place] {
 
-        
+        let places = try await loader()
+
+        self.places = places
+
+        return places
     }
 
     func fromSuggestions(filter: String) -> [Place] {
@@ -26,12 +33,12 @@ final class TripPlannerImpl: TripPlanner {
 
     func select(from: Place) throws {
 
-        
+        guard let places = places else { throw Error.notLoaded }
     }
 
     func select(to: Place) throws {
 
-        
+        guard let places = places else { throw Error.notLoaded }
     }
 
     func clearSelection() {
@@ -43,4 +50,17 @@ final class TripPlannerImpl: TripPlanner {
 
         []
     }
+
+    init(loader: @escaping Loader) {
+
+        self.loader = loader
+    }
+
+    // MARK: Private
+
+    private let loader: Loader
+
+    private var from: Place?
+    private var to: Place?
+    private var places: [Place]? = nil
 }
