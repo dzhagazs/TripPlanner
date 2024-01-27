45701205732574271376d068e3bc5eb3b3b7fadf
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-27 13:23:43 +0200

    Add test_decode_failsOnInvalidData

--- TripPlannerCore/TripPlannerCoreTests/ConnectionDecoderTests.swift
+++ TripPlannerCore/TripPlannerCoreTests/ConnectionDecoderTests.swift
@@ -34,6 +34,21 @@ final class ConnectionDecoderTests: XCTestCase {
         ])
     }
 
+    func test_decode_failsOnInvalidData() {
+
+        ensureFailsToDecode("some randome string")
+        ensureFailsToDecode("{\"connections\": {} }")
+        ensureFailsToDecode("{\"connections\": [{[]}] }")
+        ensureFailsToDecode("{\"connection\": [] }")
+        ensureFailsToDecode("{\"connections\": [{}] }")
+        ensureFailsToDecode("{\"connections\": [{ \"from\": \"\", \"to\": \"\" }] }")
+        ensureFailsToDecode("{\"connections\": [{ \"coordinates\": { \"from\": 10, \"to\": 10 } }] }")
+        ensureFailsToDecode("{\"connections\": [{ \"from\": \"\", \"to\": \"\", \"coordinates\": { \"from\": 10, \"to\": 10 } }] }")
+        ensureFailsToDecode("{\"connections\": [{ \"from\": \"\", \"to\": \"\", \"coordinates\": { \"from\": { \"lat\": 1, \"long\": 2 }, \"to\": { \"lat\": 2, \"long\": 1 } } }] }")
+        ensureFailsToDecode(encodedString(from: []) + "}")
+        ensureFailsToDecode("{" + encodedString(from: []))
+    }
+
     // MARK: Private
 
     private func makeSUT() -> SUT {
