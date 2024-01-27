9ebdcc429fd8727605360bde12421911a8d653a0
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-27 14:51:03 +0200

    Add DistanceCalculatorTests

--- TripPlannerCore/TripPlannerCoreTests/DistanceCalculatorTests.swift
+++ TripPlannerCore/TripPlannerCoreTests/DistanceCalculatorTests.swift
@@ -2,7 +2,33 @@
 //  DistanceCalculatorTests.swift
 //  TripPlannerCoreTests
 //
-//  Created by Alexandr Vasildzhagaz on 27.01.2024.
+//  Created by Oleksandr Vasildzhahaz on 27.01.2024.
 //
 
-import Foundation
+@testable import TripPlannerCore
+
+import XCTest
+
+final class DistanceCalculatorTests: XCTestCase {
+
+    typealias SUT = DistanceCalculator
+
+    func test_distance_isCalculatedCorrectly() {
+
+        let london = Coordinate(latitude: 51.5285582, longitude: -0.241681)
+        let tokyo = Coordinate(latitude: 35.652832, longitude: 139.839478)
+        let porto = Coordinate(latitude: 41.14961, longitude: -8.61099)
+
+        let distances = [
+
+            (london, tokyo, Float(9593537.0)),
+            (london, porto, Float(1319441.0)),
+            (tokyo, porto, Float(10912841.0))
+        ]
+
+        distances.forEach { connection in
+
+            XCTAssertEqual(connection.2, SUT.distance(from: connection.0, to: connection.1))
+        }
+    }
+}
