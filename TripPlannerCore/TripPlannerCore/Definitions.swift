90518581f17876424ff8c0da49b8340428329bd2
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-27 13:45:15 +0200

    Add Definitions

---
+++ TripPlannerCore/TripPlannerCore/Definitions.swift
@@ -0,0 +1,17 @@
+//
+//  Definitions.swift
+//  TripPlannerCore
+//
+//  Created by Oleksandr Vasildzhahaz on 27.01.2024.
+//
+
+import Foundation
+
+struct Definitions {
+
+    static let sourceURL = URL(string: "https://raw.githubusercontent.com/TuiMobilityHub/ios-code-challenge/master/connections.json")!
+
+    // MARK: Private
+
+    private init() {}
+}
