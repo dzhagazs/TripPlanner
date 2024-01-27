eda9ea46d65e3c9d06b8e92cf577fbe125d3c092
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-27 14:50:45 +0200

    Fix DistanceCalculator

--- TripPlannerCore/TripPlannerCore/ConnectionLoader/Impl/DistanceCalculator.swift
+++ TripPlannerCore/TripPlannerCore/ConnectionLoader/Impl/DistanceCalculator.swift
@@ -12,7 +12,7 @@ final class DistanceCalculator {
     static func distance(from: Coordinate, to: Coordinate) -> Float {
 
         let locationFrom = CLLocation(latitude: Double(from.latitude), longitude: Double(from.longitude))
-        let locationTo = CLLocation(latitude: Double(from.latitude), longitude: Double(from.longitude))
+        let locationTo = CLLocation(latitude: Double(to.latitude), longitude: Double(to.longitude))
 
         return Float(locationFrom.distance(from: locationTo))
     }
---
+++ TripPlannerCore/TripPlannerCoreTests/DistanceCalculatorTests.swift
@@ -0,0 +1,8 @@
+//
+//  DistanceCalculatorTests.swift
+//  TripPlannerCoreTests
+//
+//  Created by Alexandr Vasildzhagaz on 27.01.2024.
+//
+
+import Foundation
