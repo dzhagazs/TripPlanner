499db3c108518993cb0bfb592cc6086b71fadbfa
dzhagaz <dzhagaz.sasha@gmail.com>
2024-01-28 22:44:56 +0200

    Add TripPlannerModelTests

---
+++ TripPlanner/TripPlannerTests/TripPlanModelTests.swift
@@ -0,0 +1,28 @@
+//
+//  TripPlanModelTests.swift
+//  TripPlannerTests
+//
+//  Created by Oleksandr Vasildzhahaz on 28.01.2024.
+//
+
+@testable import TripPlanner
+
+import TripPlannerCore
+import XCTest
+
+final class TripPlanModelTests: XCTestCase {
+
+    typealias SUT = TripPlanModel
+
+    func test_some() {
+
+        
+    }
+
+    // MARK: Private
+
+    func makeSUT() -> SUT {
+
+        .init(planner: TripPlannerStub())
+    }
+}
